460f3b191c81f310ab5c843a60a443d6b480c9c3
Le Dinh Nguyen <ldnguyen2901@gmail.com>
2025-03-01 22:18:35 +0700

    Autoincrement _id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 
 const slug = require('mongoose-slug-updater');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema(
   {
+    _id: { type: Number },
     name: { type: String, maxLength: 255, required: true },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
@@ -17,10 +19,14 @@ const Course = new Schema(
     videoid: { type: String, maxLength: 255, required: true },
     level: { type: String, maxLength: 255 },
   },
-  { timestamps: true },
+  {
+    _id: false,
+    timestamps: true,
+  },
 );
 
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
   deletedAt: true,
   overrideMethods: 'all',
